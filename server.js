8228358ec3f160a4ff74f0cbb4317f1e66c8f3dc
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-06-23 22:39:58 +0800

    deploy

--- server.js
+++ server.js
@@ -4,18 +4,28 @@ const connectDB = require("./config/db");
 //initialize express
 const app = express();
 
+const path = require('path')
+
 //connect db
 connectDB();
 
 //init middleware
 app.use(express.json({ extended: false }));
 //add endpoint
-app.get("/", (req, res) => res.json({ msg: "WELCOMEEE" }));
+//app.get("/", (req, res) => res.json({ msg: "WELCOMEEE" }));
 
 //define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*'), (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+}
 
 const PORT = process.env.PORT || 5000;
 
